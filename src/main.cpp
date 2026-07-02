46805f445e77713923218d920ed38527c963a00c
davide <dado.veneziani@gmail.com>
2026-07-02 16:51:17 +0200

    Corrected wrong error message

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[]) {
   if (argc != 8) {
     std::cerr << argv[0]
-              << " needs eight entry arguments: a b c d x0 y0 steps\n";
+              << " needs seven entry arguments: a b c d x0 y0 steps\n";
   }
 
   try {
