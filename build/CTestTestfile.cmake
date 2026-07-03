8807fa431f2b980ff6058c0ede62833800438260
davide <dado.veneziani@gmail.com>
2026-07-03 12:59:39 +0200

    Ignore build files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davide/Documents/ProjectsVSC/lotka_volterra
-# Build directory: /home/davide/Documents/ProjectsVSC/lotka_volterra/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[lotka_volterra_test]=] "/home/davide/Documents/ProjectsVSC/lotka_volterra/build/lotka_volterra_test")
-set_tests_properties([=[lotka_volterra_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/davide/Documents/ProjectsVSC/lotka_volterra/CMakeLists.txt;67;add_test;/home/davide/Documents/ProjectsVSC/lotka_volterra/CMakeLists.txt;0;")
