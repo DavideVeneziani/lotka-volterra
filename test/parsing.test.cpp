6ef481cc47829d3ed7f937d395e244483c46ed01
davide <dado.veneziani@gmail.com>
2026-07-03 10:55:12 +0200

    Added parsing testing draft file

---
+++ test/parsing.test.cpp
@@ -0,0 +1,55 @@
+#include "parsing.hpp"
+
+#include "doctest.h"
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace {
+
+lv::Program_options parse(std::vector<std::string>& arguments)
+{
+  std::vector<char*> argv;
+
+  for (std::string& argument : arguments) {
+    argv.push_back(argument.data());
+  }
+
+  return lv::parse_arguments(static_cast<int>(argv.size()), argv.data());
+}
+
+} // namespace
+
+TEST_CASE("Parse required command line arguments")
+{
+  std::vector<std::string> arguments = {
+      "lotka_volterra", "1.0", "0.1", "1.5", "0.075", "10.0", "5.0", "100"};
+
+  lv::Program_options options = parse(arguments);
+
+  CHECK(options.a == doctest::Approx(1.0));
+  CHECK(options.b == doctest::Approx(0.1));
+  CHECK(options.c == doctest::Approx(1.5));
+  CHECK(options.d == doctest::Approx(0.075));
+  CHECK(options.x0 == doctest::Approx(10.0));
+  CHECK(options.y0 == doctest::Approx(5.0));
+  CHECK(options.steps == 100);
+  CHECK(options.has_output_file == false);
+  CHECK(options.precision == 2);
+}
+
+TEST_CASE("Parsing fails if one required argument is not numeric")
+{
+  std::vector<std::string> valid_arguments = {
+      "lotka_volterra", "1.0", "0.1", "1.5", "0.075", "10.0", "5.0", "100"};
+
+  for (int i = 1; i <= 7; ++i) {
+    std::vector<std::string> arguments = valid_arguments;
+    arguments[i] = "abc";
+
+    CAPTURE(i);
+
+    CHECK_THROWS_AS(parse(arguments), std::invalid_argument);
+  }
+}
--- test/simulation.test.cpp
+++ test/simulation.test.cpp
@@ -31,3 +31,32 @@ TEST_CASE("Simulation rejects non-positive parameters")
   CHECK_THROWS_AS(lv::Simulation(1.0, 2.0, 3.0, 0.0, 1.0, 1.0),
                   std::invalid_argument);
 }
+
+TEST_CASE("Simulation prints results")
+{
+  lv::Simulation simulation{1.0, 0.1, 1.5, 0.075, 10.0, 5.0};
+
+  simulation.run(3);
+
+  std::ostringstream os;
+  simulation.print(os, 2);
+
+  CHECK(!os.str().empty());
+}
+
+TEST_CASE("Simulation print uses selected precision")
+{
+  lv::Simulation simulation{1.0, 0.1, 1.5, 0.075, 10.0, 5.0};
+
+  simulation.run(1);
+
+  std::ostringstream output1;
+  std::ostringstream output2;
+  simulation.print(output1, 2);
+  simulation.print(output2, 6);
+  std::string printed_text1 = output1.str();
+  std::string printed_text2 = output2.str();
+
+  CHECK(printed_text1.find("10.00") != std::string::npos);
+  CHECK(printed_text2.find("10.000000") != std::string::npos);
+}
